49d9cd5900f4b116f822e0a4075572017c23fd64
Wojciech Bojarski <bojawojt@gmail.com>
2020-01-27 09:41:49 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
   
+    has_many :characters
+    has_many :shows, through: :characters
+    
+    def full_name
+        self.first_name + " " + self.last_name
+    end
+
+    def list_roles
+        characters = self.characters.map {|character| character.name}
+        shows = self.shows.map {|show| show.name}
+
+        characters.zip(shows).join(" - ")
+
+    end
 end
